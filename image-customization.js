cb10f60a5f5791eecaea6fc5f11ca04aa3de0153
Martijn van de Rijdt <martijn@enketo.org>
2018-12-11 14:38:33 -0700

    changed: partial conversion to new Widget format (works in Enketo Core 5.x)

--- image-customization.js
+++ image-customization.js
@@ -1,45 +1,45 @@
-import Widget from 'enketo-core/src/js/Widget';
+import Widget from 'enketo-core/src/js/widget';
 import $ from 'jquery';
-const pluginName = 'imageCustomizer';
+
 let complementaryStylesheet; // one stylesheet per widget (not per instance of the widget)
 
 /**
  * Image Map widget that turns an SVG image into a clickable map 
  * by matching radiobutton/checkbox values with id attribute values in the SVG
- *
- * @constructor
- * @param {Element}                       element   Element to apply widget to.
- * @param {(boolean|{touch: boolean})}    options   options
- * @param {*=}                            event     event
  */
-function ImageCustomizer(element, options, event) {
-    this.namespace = pluginName;
-    Widget.call(this, element, options);
-    this.options = options;
-    this._init();
+class ImageCustomizer extends Widget {
+
+    static get selector() {
+        return '.or-appearance-image-customization[data-image-customization]';
+    }
+
+    static get helpersRequired() {
+        return [ 'input', 'pathToAbsolute' ];
+    }
+
 }
 
-ImageCustomizer.prototype = Object.create(Widget.prototype);
-ImageCustomizer.prototype.constructor = ImageCustomizer;
+// TODO: move these methods inside the class statement.
+// TODO: make this widget compliant with the new Enketo Core widget format and run the common tests.
 
-ImageCustomizer.prototype._init = function () {
+ImageCustomizer.prototype._init = function() {
     const that = this;
-    const img = this.element.querySelector('img');
+    const img = this.element.querySelector( 'img' );
     this.$styleInput = this._getStyleInput();
-    this.$styleInput.on('valuechange.enketo inputupdate.enketo', this._updateImage.bind(this));
+    this.$styleInput.on( 'valuechange.enketo inputupdate.enketo', this._updateImage.bind( this ) );
 
-    if ($(this.element).data('imageMap')) {
+    if ( this.element.classList.contains( 'or-image-map-initialized' ) ) {
         // Knowing that the imagemap widget will be added to DOM as sibling of .option-wrapper, 
         // observe its parent and monitor the childList.
-        new MutationObserver(mutations => {
-            mutations.forEach(mutation => {
-                if (mutation.addedNodes[0].classList.contains('image-map')) {
+        new MutationObserver( mutations => {
+            mutations.forEach( mutation => {
+                if ( mutation.addedNodes[ 0 ].classList.contains( 'image-map' ) ) {
                     that._updateImage();
                 }
-            });
-        }).observe(that.element.querySelector('.option-wrapper').parentElement, {
+            } );
+        } ).observe( that.element.querySelector( '.option-wrapper' ).parentElement, {
             childList: true
-        });
+        } );
         return;
     }
 
@@ -50,109 +50,108 @@ ImageCustomizer.prototype._init = function () {
      *
      * We could use the same with online-only forms, but that would cause a loading delay.
      */
-    if (this.$styleInput && this.$styleInput.length && img) {
-        if (img.getAttribute('src')) {
-            this._addMarkup(img).then(this._updateImage.bind(this));
+    if ( this.$styleInput && this.$styleInput.length && img ) {
+        if ( img.getAttribute( 'src' ) ) {
+            this._addMarkup( img ).then( this._updateImage.bind( this ) );
         } else {
-            $(img)
-                .on('load', () => {
-                    that._addMarkup(img).then(that._updateImage.bind(that));
-                });
+            $( img )
+                .on( 'load', () => {
+                    that._addMarkup( img ).then( that._updateImage.bind( that ) );
+                } );
         }
     }
 
 };
 
-ImageCustomizer.prototype._getStyleInput = function () {
-    const input = this.element.querySelector('input');
-    const contextPath = this.options.helpers.input.getName($(input));
+ImageCustomizer.prototype._getStyleInput = function() {
+    const input = this.element.querySelector( 'input' );
+    const contextPath = this.options.helpers.input.getName( $( input ) );
 
-    if (this.element.dataset.imageCustomization) {
+    if ( this.element.dataset.imageCustomization ) {
         const targetPath = this.element.dataset.imageCustomization.trim();
-        const absoluteTargetPath = this.options.helpers.pathToAbsolute(targetPath, contextPath);
+        const absoluteTargetPath = this.options.helpers.pathToAbsolute( targetPath, contextPath );
         // The  code below will fail if the style node is inside a repeat (except for the first repeat instance in series)
-        const $root = $(this.element).closest('form.or');
-        return $root.find(`[name="${absoluteTargetPath}"], [data-name="${absoluteTargetPath}"]`).eq(0);
+        const $root = $( this.element ).closest( 'form.or' );
+        return $root.find( `[name="${absoluteTargetPath}"], [data-name="${absoluteTargetPath}"]` ).eq( 0 );
     }
     return $();
 };
 
-ImageCustomizer.prototype._addMarkup = function (img) {
+ImageCustomizer.prototype._addMarkup = function( img ) {
     const that = this;
-    const src = img.getAttribute('src');
+    const src = img.getAttribute( 'src' );
 
     /**
      * For translated forms, we now discard everything except the first image,
      * since we're assuming the images will be the same in all languages.
      */
-    return $.get(src).then(data => {
+    return $.get( src ).then( data => {
         let $svg;
         let $widget;
         let width;
         let height;
-        if (that._isSvgDoc(data)) {
-            $svg = $(data.querySelector('svg'));
-            $widget = $('<div class="widget image-map"/>').append($svg);
+        if ( that._isSvgDoc( data ) ) {
+            $svg = $( data.querySelector( 'svg' ) );
+            $widget = $( '<div class="widget image-map"/>' ).append( $svg );
             // remove images in all languages
-            $(that.element).find('img').remove();
-            $(that.element).append($widget);
+            $( that.element ).find( 'img' ).remove();
+            $( that.element ).append( $widget );
             // Resize, using original unscaled SVG dimensions
             // svg.getBBox() only works after SVG has been added to DOM.
-            width = $svg.attr('width') || $svg[0].getBBox().width;
-            height = $svg.attr('height') || $svg[0].getBBox().height;
-            $svg.attr('viewBox', [0, 0, width, height].join(' '));
+            width = $svg.attr( 'width' ) || $svg[ 0 ].getBBox().width;
+            height = $svg.attr( 'height' ) || $svg[ 0 ].getBBox().height;
+            $svg.attr( 'viewBox', [ 0, 0, width, height ].join( ' ' ) );
 
             return $widget;
         }
-    });
+    } );
 };
 
-ImageCustomizer.prototype._getInput = function (id) {
-    return this.element.querySelector(`input[value="${id}"]`);
+ImageCustomizer.prototype._getInput = function( id ) {
+    return this.element.querySelector( `input[value="${id}"]` );
 };
 
-ImageCustomizer.prototype._isSvgDoc = data => typeof data === 'object' && data.querySelector('svg');
+ImageCustomizer.prototype._isSvgDoc = data => typeof data === 'object' && data.querySelector( 'svg' );
 
-ImageCustomizer.prototype._complementSelectedRule = function (obj) {
+ImageCustomizer.prototype._complementSelectedRule = function( obj ) {
     // TODO: add a .or-appearance-image-customization [or-selected] CSS rule at the end of the stylesheet.
-    if (!complementaryStylesheet) {
-        $('head').append('<style/>');
-        complementaryStylesheet = document.styleSheets[document.styleSheets.length - 1];
+    if ( !complementaryStylesheet ) {
+        $( 'head' ).append( '<style/>' );
+        complementaryStylesheet = document.styleSheets[ document.styleSheets.length - 1 ];
     }
-    if (complementaryStylesheet.cssRules[0]) {
-        complementaryStylesheet.deleteRule(0);
+    if ( complementaryStylesheet.cssRules[ 0 ] ) {
+        complementaryStylesheet.deleteRule( 0 );
     }
     complementaryStylesheet.insertRule(
-        `.or-appearance-image-customization .image-map svg path[id][or-selected]{${this._getStyleValue(obj)}}`, 0);
+        `.or-appearance-image-customization .image-map svg path[id][or-selected]{${this._getStyleValue(obj)}}`, 0 );
 };
-
 
 /**
  * Updates 'selected' attributes in SVG
  * Always update the map after the value has changed in the original input elements
  */
-ImageCustomizer.prototype._updateImage = function () {
+ImageCustomizer.prototype._updateImage = function() {
     let $path;
     let id;
     let styleAttr;
     const str = this.$styleInput.val();
-    const style = JSON.parse(str);
+    const style = JSON.parse( str );
     // We do not cache this to simplify instantiation on question that also have an imagemap-widget
-    const $svg = $(this.element).find('svg');
+    const $svg = $( this.element ).find( 'svg' );
 
     // Update .or-selected style
-    this._complementSelectedRule(style.selected);
+    this._complementSelectedRule( style.selected );
 
     // Remove custom styles
-    $svg.find('path[id][style]').removeAttr('style');
+    $svg.find( 'path[id][style]' ).removeAttr( 'style' );
 
-    if (style && $svg.length) {
+    if ( style && $svg.length ) {
         // If multiple values have the same id, change all of them (e.g. a province that is not contiguous)
-        for (id in style) {
-            $path = $svg.find(`path#${id}`);
-            styleAttr = this._getStyleValue(style[id]);
+        for ( id in style ) {
+            $path = $svg.find( `path#${id}` );
+            styleAttr = this._getStyleValue( style[ id ] );
             //styleAttr = $path.is( '[or-selected]' ) && style.selected ? styleAttr + this._getStyleAttrValue( style.selected ) : styleAttr;
-            $path.attr('style', styleAttr);
+            $path.attr( 'style', styleAttr );
         }
     }
 };
@@ -160,31 +159,10 @@ ImageCustomizer.prototype._updateImage = function () {
 ImageCustomizer.prototype._getStyleValue = obj => {
     let prop;
     let value = '';
-    for (prop in obj) {
+    for ( prop in obj ) {
         value += `${prop}:${obj[prop]} !important;`;
     }
     return value;
 };
 
-
-$.fn[pluginName] = function (options, event) {
-
-    options = options || {};
-
-    return this.each(function () {
-        const $this = $(this);
-        const data = $this.data(pluginName);
-
-        if (!data && typeof options === 'object') {
-            $this.data(pluginName, new ImageCustomizer(this, options, event));
-        } else if (data && typeof options == 'string') {
-            data[options](this);
-        }
-    });
-};
-
-export default {
-    'name': pluginName,
-    'selector': '.or-appearance-image-customization[data-image-customization]',
-    'helpersRequired': ['input', 'pathToAbsolute']
-};
+export default ImageCustomizer;
